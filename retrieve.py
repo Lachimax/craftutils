5f45a40a8bdc3021446cab6054c86ce99461f33c
Lachlan <lachlan.marnoch@students.mq.edu.au>
2021-01-18 17:23:52 +1100

    DES Access

--- retrieve.py
+++ retrieve.py
@@ -3,7 +3,9 @@
 import urllib
 from datetime import date
 from typing import Union
-from astropy.coordinates import SkyCoord
+import requests
+import os
+import time
 
 from craftutils import params as p
 from craftutils import utils as u
@@ -260,3 +262,145 @@ def update_frb_irsa_extinction(frb: str):
                                            output=data_dir + "galactic_extinction.txt")
     p.add_output_values_frb(obj=frb, params=values)
     return values, ext_str
+
+
+# Dark Energy Survey database functions adapted code by T. Andrew Manning, from
+# https://github.com/des-labs/desaccess-docs/blob/master/_static/DESaccess_API_example.ipynb
+
+des_url = 'https://des.ncsa.illinois.edu'
+des_api_url = des_url + "/desaccess/api"
+des_files_url = des_url + "/files-desaccess"
+
+
+def des_login():
+    """
+    Obtains an auth token using the username and password credentials for a given database.
+    """
+    # Login to obtain an auth token
+    r = requests.post(
+        f'{des_api_url}/login',
+        data={
+            'username': keys['des_user'],
+            'password': keys['des_pwd'],
+            'database': keys['database']
+        }
+    )
+    # Store the JWT auth token
+    keys['des_auth_token'] = r.json()['token']
+    return keys['des_auth_token']
+
+
+def des_check_auth_token():
+    if 'des_auth_token' not in keys:
+        raise KeyError("Use des_login() to log in before submitting requests.")
+
+
+def des_submit_cutout_job(data: dict):
+    """Submits a query job and returns the complete server response which includes the job ID."""
+
+    des_check_auth_token()
+
+    # Submit job
+    r = requests.put(
+        f'{des_api_url}/job/cutout',
+        data=data,
+        headers={'Authorization': f'Bearer {keys["auth_token"]}'}
+    )
+    response = des_check_success(r)
+
+    return response
+
+
+def des_check_success(response: requests.Response):
+    response = response.json()
+
+    if response['status'] == 'ok':
+        job_id = response['jobid']
+        print('Job "{}" submitted.'.format(job_id))
+        # Refresh auth token
+        keys['des_auth_token'] = response['new_token']
+    else:
+        job_id = None
+        print('Error submitting job: '.format(response['message']))
+    return response
+
+
+def des_submit_query_job(query):
+    """Submits a query job and returns the complete server response which includes the job ID."""
+
+    des_check_auth_token()
+
+    # Specify API request parameters
+    data = {
+        'username': keys['des_user'],
+        'db': 'desdr',
+        'filename': 'DES.csv',
+        'query': query
+    }
+
+    # Submit job
+    r = requests.put(
+        f'{des_api_url}/job/query',
+        data=data,
+        headers={'Authorization': f'Bearer {keys["des_auth_token"]}'}
+    )
+    response = des_check_success(response=r)
+
+    return response
+
+
+def des_get_job_status(job_id):
+    """Returns the current status of the job identified by the unique job_id."""
+
+    des_check_auth_token()
+
+    r = requests.post(
+        f'{des_api_url}/job/status',
+        data={
+            'job-id': job_id
+        },
+        headers={'Authorization': f'Bearer {keys["des_auth_token"]}'}
+    )
+    response = r.json()
+    # Refresh auth token
+    keys['des_auth_token'] = response['new_token']
+    # print(json.dumps(response, indent=2))
+    return response
+
+
+def des_job_status_poll(job_id):
+    print(f'Polling status of job "{job_id}"...', end='')
+    job_status = ''
+    response = None
+    while job_status != 'ok':
+        # Fetch the current job status
+        response = des_get_job_status(job_id)
+        # Quit polling if there is an error getting a status update
+        if response['status'] != 'ok':
+            break
+        job_status = response['jobs'][0]['job_status']
+        if job_status == 'success' or job_status == 'failure':
+            print(f'\nJob completed with status: {job_status}')
+            break
+        else:
+            # Display another dot to indicate that polling is still active
+            print('.', end='', sep='', flush=True)
+        time.sleep(3)
+    return response
+
+
+def des_download_job_files(url, outdir):
+    os.makedirs(outdir, exist_ok=True)
+    r = requests.get(f'{url}/json')
+    for item in r.json():
+        if item['type'] == 'directory':
+            suburl = f'{url}/{item["name"]}'
+            subdir = f'{outdir}/{item["name"]}'
+            des_download_job_files(suburl, subdir)
+        elif item['type'] == 'file':
+            data = requests.get('{}/{}'.format(url, item['name']))
+            with open('{}/{}'.format(outdir, item['name']), "wb") as file:
+                file.write(data.content)
+
+    response = r.json()
+    return response
